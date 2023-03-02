c75a641efc072fe80526e9832c4614c3e06938d7
yasinabdmahmood <abd181103@gmail.com>
2023-03-02 13:31:50 +0300

    Ran migrations

--- db/migrate/20230302101126_create_create_subscription_types.rb
+++ db/migrate/20230302101126_create_create_subscription_types.rb
@@ -1,6 +1,6 @@
 class CreateCreateSubscriptionTypes < ActiveRecord::Migration[7.0]
   def change
-    create_table :create_subscription_types do |t|
+    create_table :subscription_types do |t|
       t.string :type
       t.decimal :cost
       t.decimal :profit
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_090932) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_102319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "client_contact_informations", force: :cascade do |t|
+    t.string "contact_information"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_client_contact_informations_on_client_id"
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employee_contact_informations", force: :cascade do |t|
     t.string "contact_info"
@@ -29,5 +44,40 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_090932) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "payment_records", force: :cascade do |t|
+    t.bigint "subscription_record_id", null: false
+    t.bigint "employee_id", null: false
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_payment_records_on_employee_id"
+    t.index ["subscription_record_id"], name: "index_payment_records_on_subscription_record_id"
+  end
+
+  create_table "subscription_records", force: :cascade do |t|
+    t.bigint "client_id", null: false
+    t.bigint "subscription_type_id", null: false
+    t.bigint "employee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_subscription_records_on_client_id"
+    t.index ["employee_id"], name: "index_subscription_records_on_employee_id"
+    t.index ["subscription_type_id"], name: "index_subscription_records_on_subscription_type_id"
+  end
+
+  create_table "subscription_types", force: :cascade do |t|
+    t.string "type"
+    t.decimal "cost"
+    t.decimal "profit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "client_contact_informations", "clients"
   add_foreign_key "employee_contact_informations", "employees", column: "employees_id"
+  add_foreign_key "payment_records", "employees"
+  add_foreign_key "payment_records", "subscription_records"
+  add_foreign_key "subscription_records", "clients"
+  add_foreign_key "subscription_records", "employees"
+  add_foreign_key "subscription_records", "subscription_types"
 end
