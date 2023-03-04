5f2722e879836bfc807dc582d3b0070f39a8c620
yasinabdmahmood <abd181103@gmail.com>
2023-03-04 22:22:39 +0300

    Add subscription type deletion functionality

--- app/controllers/subscription_type_controller.rb
+++ app/controllers/subscription_type_controller.rb
@@ -2,4 +2,31 @@ class SubscriptionTypeController < ApplicationController
   def index
     @subscription_types = SubscriptionType.all
   end
+
+  def new
+    @new_subscription_type = SubscriptionType.new
+  end
+
+  def destroy
+    @subscription_type = SubscriptionType.find(params[:id])
+    @subscription_type.destroy
+
+    redirect_to subscription_type_index_path, notice: "User was successfully deleted."
+  end
+
+  
+  def create
+    @new_subscription_type = SubscriptionType.new(subscription_type_params)
+    if @new_subscription_type.save
+      redirect_to subscription_type_index_path
+    else
+      redirect_to :new
+    end
+  end
+
+  private
+
+  def subscription_type_params
+    params.require(:new_subscription_type).permit(:category, :cost, :profit)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get 'payment_record/index'
-  get 'subscription_record/index'
-  get 'subscription_type/index'
-  get 'client/index'
   root "employee#index"
+
+  get 'payment_record/index'
+
+  get 'subscription_record/index'
+  
+  get '/subscription_type/index'
+  get '/subscription_type/new'
+  get '/subscription_type/:id' => 'subscription_type#destroy', as: 'destroy_subscription_type'
+  post 'subscription_type/create'
+ 
+
+  get 'client/index'
+ 
   get 'employee/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
