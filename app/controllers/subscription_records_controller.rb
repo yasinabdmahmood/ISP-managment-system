21b17310b5c3f0375a50316b54cae289d112b3c4
yasinabdmahmood <abd181103@gmail.com>
2023-03-06 16:55:33 +0300

    Add form to create new subscription record

--- app/controllers/subscription_records_controller.rb
+++ app/controllers/subscription_records_controller.rb
@@ -2,4 +2,14 @@ class SubscriptionRecordsController < ApplicationController
   def index
     @subscription_records = SubscriptionRecord.all.includes(:client, :subscription_type, :employee)
   end
+
+  def new
+    @new_subscription_record = SubscriptionRecord.new
+    @subscription_types = SubscriptionType.all
+    @clients = Client.all 
+  end
+
+  # def subscription_record_params
+  #   params.require(:product).permit(:pay, :client_id, :subscription_type_id)
+  # end
 end
