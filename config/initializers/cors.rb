722814fb00ed25683caee9abc0c8a5c088820b6f
yasinabdmahmood <abd181103@gmail.com>
2023-03-12 21:12:31 +0300

    Add cors.rb file

---
+++ config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
+  end
+end
