d596ee76169d41e39bed07f3bf6326ec1411e039
yasinabdmahmood <abd181103@gmail.com>
2023-03-12 22:50:25 +0300

    Modify index action in EmployeeController to send json data instead of rendering view

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,5 +1,6 @@
 class EmployeeController < ApplicationController
   def index
     @employees = Employee.all.includes(:employee_contact_information)
+    render json: @employees, include: { employee_contact_information: { only: [:contact_info] } }
   end
 end
