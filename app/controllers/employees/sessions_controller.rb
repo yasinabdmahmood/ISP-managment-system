6714c6297be1db271cb3fa6f5a29dd50c0bf0982
yasinabdmahmood <abd181103@gmail.com>
2023-03-13 16:33:05 +0300

    Overwrite the create action of session controller

--- app/controllers/employees/sessions_controller.rb
+++ app/controllers/employees/sessions_controller.rb
@@ -5,14 +5,25 @@ class Employees::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
 
   def create
-    super do |employee|
-      if employee.role == 'admin'
-        cookies[:role] = "admin"
-      else
-        cookies[:role] = "employee"
-      end
-    end
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    
+    render json: resource.as_json(only: [:id, :name, :email, :role])
   end
+  
+
+  # def create
+  #   super do |employee|
+  #     if employee.role == 'admin'
+  #       cookies[:role] = "admin"
+  #     else
+  #       cookies[:role] = "employee"
+  #     end
+  #     render json: employee, status: :created
+  #   end
+  # end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
