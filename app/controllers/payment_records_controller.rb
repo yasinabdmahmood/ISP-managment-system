4e2f3e544600c2aa7e2e12c7df6df7b8013238d3
yasinabdmahmood <abd181103@gmail.com>
2023-03-26 12:19:27 +0300

    Modify index action of payment record to account for infinite scroling

--- app/controllers/payment_records_controller.rb
+++ app/controllers/payment_records_controller.rb
@@ -1,7 +1,17 @@
 class PaymentRecordsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
-    @payment_records = PaymentRecord.all.includes(:subscription_record => [:client], :employee => [])
+    fetched_items_at_once = 20
+    n = params[:offset].to_i 
+    
+    total_records = PaymentRecord.count
+    
+    offset = fetched_items_at_once*n
+
+    if offset > total_records-fetched_items_at_once
+      render json: []
+    end
+    @payment_records = PaymentRecord.order(created_at: :desc).includes(:subscription_record => [:client], :employee => []).offset(offset).limit(fetched_items_at_once)
     render json: @payment_records, include: { 
       employee: { only: [:name] }, 
       subscription_record: {
@@ -26,7 +36,14 @@ class PaymentRecordsController < ApplicationController
     if @new_payment_record.amount + current_subscription_record_pay <= subscription_fee 
       @new_payment_record.save
       @new_payment_record.subscription_record.update(pay: @new_payment_record.subscription_record.pay += @new_payment_record.amount)
-      render json: {message: 'sucsess'}
+      render json: @new_payment_record, include: { 
+      employee: { only: [:name] }, 
+      subscription_record: {
+        include: {
+          client: { },
+        }
+      }
+    }
     else
       render json: {message: 'error'}
     end
