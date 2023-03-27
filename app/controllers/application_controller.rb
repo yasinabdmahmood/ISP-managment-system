8e3b310863df323a74546c25df1418e4e878e93e
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 05:18:12 +0300

    Remove seed command

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
   before_action :authenticate_employee!
+
+  before_action :set_cors_headers
+
+  private
+
+  def set_cors_headers
+    response.set_header "Access-Control-Allow-Origin", origin
+  end
+
+  def origin
+    request.headers["Origin"] || "*"
+  end
+end
 
   protected
 
