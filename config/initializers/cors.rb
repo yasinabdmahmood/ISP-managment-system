aaeb93140b0eedea805ee947c634f638f773142d
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 13:01:50 +0300

    Add remote link to cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,4 +4,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
       expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
   end
+
+  allow do
+    origins 'https://isp-system.onrender.com'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
+  end
+
 end
