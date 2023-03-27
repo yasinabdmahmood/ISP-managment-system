75b48dff3a9f682be2673189c29d9d157771e8cd
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 01:31:09 +0300

    Update CORS configuration to expose _isp_managment_system_session cookie

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module ISPManagmentSystem
       allow do
         origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
-          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional headers you want to expose here
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session'] # Add any additional headers you want to expose here
       end
     end
     
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
-        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session'] # Add any additional cookies you want to share here
     end
-  end
+end
