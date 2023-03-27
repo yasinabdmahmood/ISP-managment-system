e723542b6a85264c8ed90de835cb831f764b887f
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 14:35:43 +0300

    Delete cors.rb file

--- config/initializers/cors.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://6420f754960c8a37a162551d--aesthetic-centaur-fad1cd.netlify.app'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional headers you want to expose here
-  end
-end
