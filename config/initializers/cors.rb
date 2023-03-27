ad32c437ff9878fd72febcb96eec6b8b11f150b2
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 13:30:30 +0300

    Configure cors.rb to accept request from any origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,4 +11,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
   end
 
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
+  end
+
 end
