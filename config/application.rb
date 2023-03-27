dc2aeb5a84e369d3abc3304c688e6240e8fe6648
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 00:08:53 +0300

    Config cors policy to allow for two different origins

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ISPManagmentSystem
     config.active_record.default_timezone = :local
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional headers you want to expose here
       end
