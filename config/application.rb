524471b0f70c4eaee5ebdfaa046b1f76bc329899
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 14:29:59 +0300

    Change cors.rb in the application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,9 @@ module ISPManagmentSystem
     config.active_record.default_timezone = :local
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+        origins 'https://6420f754960c8a37a162551d--aesthetic-centaur-fad1cd.netlify.app'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional headers you want to expose here
       end
     end
     
