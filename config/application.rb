b17c165481bc0c7550e23d735cc1e58b07ac4e9d
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 01:57:40 +0300

    Add another origin to cors config

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ISPManagmentSystem
     config.active_record.default_timezone = :local
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app'
+        origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app', 'https://isp-system-react.onrender.com'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session'] # Add any additional headers you want to expose here
       end
