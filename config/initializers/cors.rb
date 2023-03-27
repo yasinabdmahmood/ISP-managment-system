6437bd5ccc82320c19bc3a2c9c31dc6cead393e7
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 14:22:44 +0300

    Change cors.rb to have one link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,22 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-
   allow do
     origins 'https://6420f754960c8a37a162551d--aesthetic-centaur-fad1cd.netlify.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional headers you want to expose here
   end
-  
-  allow do
-    origins 'http://localhost:3001'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'] # Add any additional cookies you want to share here
-  end
-
-  
-
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: false
-  end
-
 end
