e7d293ffa7fa7a2f35b41dd8d80b0e4064436920
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 14:54:42 +0300

    Delete some cors realted code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_employee!
 
-  before_action :set_cors_headers
-
-  private
-
-  def set_cors_headers
-    response.set_header "Access-Control-Allow-Origin", origin
-  end
-
-  def origin
-    request.headers["Origin"] || "*"
-  end
-
+  
 
   protected
 
