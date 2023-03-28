bb8bc782e99ec33043d6d496ec858aabb45d117d
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 14:07:03 +0300

    Set same_site attribute to none in cors configutation

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app', 'https://isp-system-react.onrender.com'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
-        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session'] # Add any additional cookies you want to share here
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session'],
+        :credentials => true, 
+        :same_site => :none,
+        :secure => true
     end
 end
