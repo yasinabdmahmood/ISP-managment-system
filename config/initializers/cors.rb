0e8e77fc6ddca8b778bbe9f7e0b79a48cf87a387
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 14:17:39 +0300

    Set-Cookie to expose array in cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app', 'https://isp-system-react.onrender.com'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
-        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session'],
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session', 'Set-Cookie'],
         :credentials => true, 
         :same_site => :none,
         :secure => true
