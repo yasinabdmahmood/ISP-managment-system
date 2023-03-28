b90c9d6bb81a4fb679709257bcc5a7fbceaaccc8
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 15:14:37 +0300

    Fix bug in cors configuration6

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,9 @@
-# config/application.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://isp-system-react.onrender.com'
-    resource '*', 
-      headers: :any, 
-      methods: [:get, :post, :put, :delete],
-      credentials: true,
-      expose: ['Set-Cookie'],
-      max_age: 600,
-      :same_site => :none,
-      :secure => true
-  end
+    allow do
+      origins 'https://isp-react.netlify.app', 'https://642201687183b1000808f0d5--isp-react.netlify.app', 'https://isp-system-react.onrender.com'
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session', 'Set-Cookie'],
+        :same_site => :none,
+        :secure => true
+    end
 end
