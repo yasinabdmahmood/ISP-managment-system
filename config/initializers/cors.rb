f2f9b9bbe7f63167f51833bf83b058663951891d
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 15:01:10 +0300

    Fix bug in cors configuration5

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,14 +2,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://isp-system-react.onrender.com'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :delete],
       credentials: true,
       expose: ['Set-Cookie'],
       max_age: 600,
-      same_site: 'none',
-      secure: true
+      :same_site => :none,
+      :secure => true
   end
 end
