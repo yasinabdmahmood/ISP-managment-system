2a5172dca2cdc60c7a6b1db33de93a97d80f6f87
yasinabdmahmood <abd181103@gmail.com>
2023-03-30 14:14:20 +0300

    Add controller action to handle updating employee information

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,6 +1,23 @@
 class EmployeeController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @employees = Employee.all.includes(:employee_contact_information)
     render json: @employees, include: { employee_contact_information: { only: [:id, :contact_info] } }
   end
+  # employee/update
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update(employee_params)
+      render json: { message: 'Success' }, status: 200
+    else
+      render json: {message: 'error'}, status: 400
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
