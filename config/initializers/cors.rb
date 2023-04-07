99ded86980765a88fcf4daa4d102133710881ba9
yasinabdmahmood <abd181103@gmail.com>
2023-04-07 15:35:50 +0300

    Add another origin to cord config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://isp-react.netlify.app', 'http://localhost:3001', 'https://isp-system-react.onrender.com'
+      origins 'https://isp-react.netlify.app', 'http://localhost:3001', 'https://isp-system-react.onrender.com', 'https://baban-net-frontend.herokuapp.com'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options], credentials: true,
         expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_isp_managment_system_session', 'Set-Cookie'],
         :same_site => :none,
