2c9a42cd1531800b7419b8db9744169c04221e55
yasinabdmahmood <abd181103@gmail.com>
2023-04-08 14:39:21 +0300

    Undo fix iphone issue 1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,7 @@
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, 
-    key: '_isp_managment_system_session', 
-    domain: 'isp-system-react.onrender.com',
-    path: '/',
-    same_site: :none,
-    secure: :true
-else
-  Rails.application.config.session_store :cookie_store, 
-    key: '_isp_managment_system_session'
+if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENVIRONMENT'] == 'production'
+  Rails.application.config.session_store :cookie_store, :key => '_isp_managment_system_session',
+  :domain => :all,
+  :same_site => :none,
+  :secure => :true,
+  :tld_length => 3
 end
