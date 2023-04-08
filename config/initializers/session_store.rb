cdb2494279a475d608f387c038eb796636003375
yasinabdmahmood <abd181103@gmail.com>
2023-04-08 14:32:50 +0300

    Fix iphone issue 1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,11 @@
-if ENV['RAILS_ENV'] == 'production' || ENV['RAILS_ENVIRONMENT'] == 'production'
-  Rails.application.config.session_store :cookie_store, :key => '_isp_managment_system_session',
-  :domain => :all,
-  :same_site => :none,
-  :secure => :true,
-  :tld_length => 3
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, 
+    key: '_isp_managment_system_session', 
+    domain: 'isp-system-react.onrender.com',
+    path: '/',
+    same_site: :none,
+    secure: :true
+else
+  Rails.application.config.session_store :cookie_store, 
+    key: '_isp_managment_system_session'
 end
