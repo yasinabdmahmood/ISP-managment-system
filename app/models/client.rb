025fd2685e5c359052566f7fccc15ffd7a772bd3
yasinabdmahmood <abd181103@gmail.com>
2023-04-10 11:08:00 +0300

    Add validation to coordinate field

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,8 @@ class Client < ApplicationRecord
     has_many :client_contact_informations, dependent: :destroy
     accepts_nested_attributes_for :client_contact_informations
     has_many :subscription_records, dependent: :destroy
-
+    
+    validates :coordinate, format: { with: /\A-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?\z/, message: "must be a valid GPS coordinate" }
     validates_associated :client_contact_informations
     validates_associated :subscription_records
     validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
