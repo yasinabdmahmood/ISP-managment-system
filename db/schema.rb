cb984ddd64010bcf4bef500db086ec4257a39195
yasinabdmahmood <abd181103@gmail.com>
2023-04-14 17:35:34 +0300

    changes schema file after runnig migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_075357) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_223220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_075357) do
     t.boolean "is_fully_paid"
     t.decimal "pay"
     t.text "note"
+    t.decimal "cost"
+    t.string "category"
     t.index ["client_id"], name: "index_subscription_records_on_client_id"
     t.index ["employee_id"], name: "index_subscription_records_on_employee_id"
     t.index ["subscription_type_id"], name: "index_subscription_records_on_subscription_type_id"
