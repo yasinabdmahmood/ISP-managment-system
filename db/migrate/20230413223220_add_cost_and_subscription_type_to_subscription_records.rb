bcc857ad1af48de27c16a43cd125685f07c324d9
yasinabdmahmood <abd181103@gmail.com>
2023-04-14 17:34:40 +0300

    Add migration to add new fields to subscription record containing the cost and category of the subscription record

---
+++ db/migrate/20230413223220_add_cost_and_subscription_type_to_subscription_records.rb
@@ -0,0 +1,11 @@
+class AddCostAndSubscriptionTypeToSubscriptionRecords < ActiveRecord::Migration[7.0]
+  def change
+    add_column :subscription_records, :cost, :decimal
+    add_column :subscription_records, :category, :string
+
+    # Set values for new columns based on associated subscription_type
+    SubscriptionRecord.all.each do |record|
+      record.update(cost: record.subscription_type.cost, category: record.subscription_type.category)
+    end
+  end
+end
