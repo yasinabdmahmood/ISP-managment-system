3dfa40f2906e8d6a6dd50aa8b7d625f82027afeb
yasinabdmahmood <abd181103@gmail.com>
2023-06-02 12:01:37 +0300

    Add callback method to store new actitvity record upon creating new clinet

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 class Client < ApplicationRecord
+
+    after_create :add_client_to_activity
+
     has_many :client_contact_informations, dependent: :destroy
     accepts_nested_attributes_for :client_contact_informations
     has_many :subscription_records, dependent: :destroy
@@ -7,5 +10,18 @@ class Client < ApplicationRecord
     validates_associated :subscription_records
     validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
     validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+
+    private
+
+    def add_client_to_activity
+        Activity.create(
+            employee_name: self.name,
+            action_type: 'create',
+            table_name: 'client',
+            json_data: self.to_json
+        )
+    end
+
+    
     
 end
