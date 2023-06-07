023c718d71703a6c5075289e0e8854ed7c420416
yasinabdmahmood <abd181103@gmail.com>
2023-06-07 21:55:12 +0300

    Add callbacks to subscription types  model class to store actions in the activity table

--- app/models/subscription_type.rb
+++ app/models/subscription_type.rb
@@ -6,4 +6,50 @@ class SubscriptionType < ApplicationRecord
     validates :category, presence: true, uniqueness: true
     validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :profit, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+    after_create :save_new_record_to_activity
+    after_destroy :save_deleted_record_to_activity
+    after_update :save_record_changes_to_activity
+
+    private
+
+        def save_new_record_to_activity
+            create_activity_record(action_type: 'create' ,table_name: 'subscription type' ,json_data: self)
+            # Activity.create(
+            #     employee_name: Current.employee.name,
+            #     action_type: 'create',
+            #     table_name: 'client',
+            #     json_data: self.to_json
+            # )
+        end
+
+        def save_deleted_record_to_activity
+            create_activity_record(action_type: 'delete' ,table_name: 'subscription type' ,json_data: self)
+            # Activity.create(
+            #     employee_name: Current.employee.name,
+            #     action_type: 'delete',
+            #     table_name: 'client',
+            #     json_data: self.to_json
+            # )
+        end
+
+        def save_record_changes_to_activity
+            changes_made = self.saved_changes
+            create_activity_record(action_type: 'update' ,table_name: 'subscription type' ,json_data: changes_made)
+            # Activity.create(
+            #     employee_name: Current.employee.name,
+            #     action_type: 'update',
+            #     table_name: 'client',
+            #     json_data: changes_made.to_json
+            # )
+        end
+
+        def create_activity_record( action_type: ,table_name: ,json_data: )
+            Activity.create(
+                employee_name: Current.employee.name,
+                action_type: action_type,
+                table_name: table_name,
+                json_data: json_data.to_json
+            )
+        end
 end
