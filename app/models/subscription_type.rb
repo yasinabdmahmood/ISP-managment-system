30929211d2e0dac37af2b043c693049a689422d1
yasinabdmahmood <abd181103@gmail.com>
2023-06-11 13:14:57 +0300

    Modify the subscription types model class callbacks

--- app/models/subscription_type.rb
+++ app/models/subscription_type.rb
@@ -14,7 +14,14 @@ class SubscriptionType < ApplicationRecord
     private
 
         def save_new_record_to_activity
-            create_activity_record(action_type: 'create' ,table_name: 'subscription type' ,json_data: self)
+            json_data = {
+                category: self.category,
+                cost: self.cost,
+                profit: self.profit,
+                created_at: self.created_at,
+                updated_at: self.updated_at,
+            }
+            create_activity_record(action_type: 'create' ,table_name: 'subscription type' ,json_data: json_data)
             # Activity.create(
             #     employee_name: Current.employee.name,
             #     action_type: 'create',
@@ -24,7 +31,14 @@ class SubscriptionType < ApplicationRecord
         end
 
         def save_deleted_record_to_activity
-            create_activity_record(action_type: 'delete' ,table_name: 'subscription type' ,json_data: self)
+            json_data = {
+                category: self.category,
+                cost: self.cost,
+                profit: self.profit,
+                created_at: self.created_at,
+                updated_at: self.updated_at,
+            }
+            create_activity_record(action_type: 'delete' ,table_name: 'subscription type' ,json_data: json_data)
             # Activity.create(
             #     employee_name: Current.employee.name,
             #     action_type: 'delete',
@@ -35,6 +49,14 @@ class SubscriptionType < ApplicationRecord
 
         def save_record_changes_to_activity
             changes_made = self.saved_changes
+            json_data = {
+                category: self.category,
+                cost: self.cost,
+                profit: self.profit,
+                created_at: self.created_at,
+                updated_at: self.updated_at,
+            }
+            merged_hash = json_data.merge(changes_made)
             create_activity_record(action_type: 'update' ,table_name: 'subscription type' ,json_data: changes_made)
             # Activity.create(
             #     employee_name: Current.employee.name,
