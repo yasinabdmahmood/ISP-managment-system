ff30fced8664335f1016a344c250c6c38047901b
yasinabdmahmood <abd181103@gmail.com>
2023-06-12 22:01:35 +0300

    Add commented code for implemtning the the activity saving feature in the employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -12,5 +12,73 @@ class Employee < ApplicationRecord
     validates_associated :payment_records
     validates :name, uniqueness: true, presence: true, length: { minimum: 3, maximum: 30 }
     validates :role, presence: true, inclusion: { in: %w(admin employee) }
+
+    # after_create :save_new_record_to_activity
+    # after_destroy :save_deleted_record_to_activity
+    # after_update :save_record_changes_to_activity
+
+    # private
+
+    # def save_new_record_to_activity
+    #     json_data = {
+    #       name: self.name,
+    #       email: self.email,
+    #       role: self.role,
+    #       created_at: self.created_at,
+    #       updated_at: self.updated_at
+    #     }
+    #     create_activity_record(action_type: 'create' ,table_name: 'Employee' ,json_data: json_data)
+    #     # Activity.create(
+    #     #     employee_name: Current.employee.name,
+    #     #     action_type: 'create',
+    #     #     table_name: 'client',
+    #     #     json_data: self.to_json
+    #     # )
+    # end
+
+    # def save_deleted_record_to_activity
+    #     json_data = {
+    #       name: self.name,
+    #       email: self.email,
+    #       role: self.role,
+    #       created_at: self.created_at,
+    #       updated_at: self.updated_at
+    #     }
+    #     create_activity_record(action_type: 'delete' ,table_name: 'Employee' ,json_data: json_data)
+    #     # Activity.create(
+    #     #     employee_name: Current.employee.name,
+    #     #     action_type: 'delete',
+    #     #     table_name: 'client',
+    #     #     json_data: self.to_json
+    #     # )
+    # end
+
+    # def save_record_changes_to_activity
+    #     changes_made = self.saved_changes
+    #     json_data = {
+    #       name: self.name,
+    #       email: self.email,
+    #       role: self.role,
+    #       created_at: self.created_at,
+    #       updated_at: self.updated_at
+    #     }
+    #     merged_hash = json_data.merge(changes_made)
+    #     create_activity_record(action_type: 'update' ,table_name: 'Employee' ,json_data: merged_hash)
+    #     # Activity.create(
+    #     #     employee_name: Current.employee.name,
+    #     #     action_type: 'update',
+    #     #     table_name: 'client',
+    #     #     json_data: changes_made.to_json,
+    #     # )
+    # end
+
+    # def create_activity_record( action_type: ,table_name: ,json_data: )
+    #     Activity.create(
+    #         employee_name: Current.employee.name,
+    #         action_type: action_type,
+    #         table_name: table_name,
+    #         json_data: json_data.to_json
+    #     )
+    # end
     
 end
