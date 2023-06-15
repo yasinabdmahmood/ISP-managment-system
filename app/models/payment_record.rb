a063c321bef1c22178730ee25c294b75e99d2dce
yasinabdmahmood <abd181103@gmail.com>
2023-06-15 23:24:03 +0300

    Remove some commented code

--- app/models/payment_record.rb
+++ app/models/payment_record.rb
@@ -51,12 +51,6 @@ class PaymentRecord < ApplicationRecord
             updated_at: self.updated_at,
         }
         create_activity_record(action_type: 'delete' ,table_name: 'payment' ,json_data: json_data)
-        # Activity.create(
-        #     employee_name: Current.employee.name,
-        #     action_type: 'delete',
-        #     table_name: 'client',
-        #     json_data: self.to_json
-        # )
     end
 
 
