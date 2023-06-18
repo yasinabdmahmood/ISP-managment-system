feb2d314e840d51f243e55b007ab89a7fa321bae
yasinabdmahmood <abd181103@gmail.com>
2023-06-19 00:05:53 +0300

    Edit backgroun job code

--- app/jobs/my_background_job.rb
+++ app/jobs/my_background_job.rb
@@ -4,5 +4,8 @@ class MyBackgroundJob < ApplicationJob
   def perform
     contact_information = EmployeeContactInformation.new(contact_info: '07501111111', employee_id: 1);
     contact_information.save
+    p '0000000000000000000000000000'
+    p Employee.first
+    p '0000000000000000000000000000'
   end
 end
