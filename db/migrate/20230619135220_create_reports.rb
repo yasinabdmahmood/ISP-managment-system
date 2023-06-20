e26e6d7c72f8c695988bebb50cca9f5b86f5a1a7
yasinabdmahmood <abd181103@gmail.com>
2023-06-20 09:31:52 +0300

    Delete Report table

--- db/migrate/20230619135220_create_reports.rb
+++
@@ -1,9 +0,0 @@
-class CreateReports < ActiveRecord::Migration[7.0]
-  def change
-    create_table :reports do |t|
-      t.jsonb :data, null: false, default: '{}'
-      t.string :report_type
-      t.timestamps
-    end
-  end
-end
