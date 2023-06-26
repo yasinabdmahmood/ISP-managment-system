18770da03cde921b9f0ad1fcdccfd5e86afa0578
yasinabdmahmood <abd181103@gmail.com>
2023-06-26 21:46:43 +0300

    Remove some commneted code2

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -45,12 +45,6 @@ class Employee < ApplicationRecord
     #       updated_at: self.updated_at
     #     }
     #     create_activity_record(action_type: 'delete' ,table_name: 'Employee' ,json_data: json_data)
-    #     # Activity.create(
-    #     #     employee_name: Current.employee.name,
-    #     #     action_type: 'delete',
-    #     #     table_name: 'client',
-    #     #     json_data: self.to_json
-    #     # )
     # end
 
     # def save_record_changes_to_activity
