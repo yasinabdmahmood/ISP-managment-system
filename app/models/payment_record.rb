f0fb86174e2cfd92007daaa4efae6ec20dabc501
yasinabdmahmood <abd181103@gmail.com>
2023-09-27 14:15:28 +0300

    Fix som minor bugs in payment_record model

--- app/models/payment_record.rb
+++ app/models/payment_record.rb
@@ -187,7 +187,6 @@ class PaymentRecord < ApplicationRecord
     end
 
     def create_empty_daily_record_for_today
-        byebug
         today = DateTime.now
 
         todays_report = DailyReport.create(
