64e8c143c0593898d5c1277567b0c077b8f1bb66
yasinabdmahmood <abd181103@gmail.com>
2023-10-05 21:20:56 +0300

    Implement get_monthly_report action of report controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,12 +6,11 @@ class ReportController < ApplicationController
         month = params[:date][:month].to_i
         day = params[:date][:day].to_i
       
-        # Create a Date object from the parameters
-        # date_to_query = Date.new(year, month, day)
-        date_to_query = Date.parse("#{year}-#{month}-#{day}")
-       
-        # Find the first daily report created on the specified date
-        daily_report = DailyReport.find_by(created_at: date_to_query.beginning_of_day..date_to_query.end_of_day)
+        # Create a Date object for the first day of the specified month and year
+        date_to_query = Date.new(year, month, 1)
+
+        # Find the monthly report created in the specified month and year
+        monthly_report = MonthlyReport.find_by(year: year, month: month)
       
         if daily_report
           render json: daily_report
@@ -21,9 +20,24 @@ class ReportController < ApplicationController
       end
 
       def get_monthly_report
+        # Extract year and month from params
         year = params[:date][:year].to_i
         month = params[:date][:month].to_i
+      
+        # Create a Date object for the first day of the specified month and year
+        start_date = Date.parse("#{year}-#{month}-1")
+        end_date = Date.parse("#{year}-#{month}-28")
+      
+        # Find the monthly report created in the specified month and year
+        monthly_report = MonthlyReport.find_by(created_at: start_date.beginning_of_day..end_date.end_of_day)
+      
+        if monthly_report
+          render json: monthly_report
+        else
+          render json: { error: "No monthly report found for the specified year and month" }, status: :not_found
+        end
       end
+      
       
 
 end
