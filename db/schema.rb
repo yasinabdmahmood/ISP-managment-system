99d6fbd47fb554fdd1cce05aa5359a9c1ecccc1b
yasinabdmahmood <abd181103@gmail.com>
2023-10-11 01:24:44 +0300

    Add Company model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_193335) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_221427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_193335) do
     t.datetime "updated_at", null: false
     t.string "coordinate"
     t.text "info"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "daily_reports", force: :cascade do |t|
