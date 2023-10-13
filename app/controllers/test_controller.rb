f2da3f3c96f4e8aa3b7c85007540709a6a542e5d
yasinabdmahmood <abd181103@gmail.com>
2023-10-13 16:54:15 +0300

    Implement test action of Test controller

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,5 +1,16 @@
 class TestController < ApplicationController
     def test 
-        render json: {data: "Test action responded successfully"}
+        requested_data = params[:requested_data]
+
+        case requested_data 
+        when "company"    
+            render json: Company.all
+        when "ledger"    
+            render json: Ledger.all
+        when "agent"    
+            render json: Agent.all
+        else
+          render json: {data: "You did not ask for any data"}
+        end
     end
 end
