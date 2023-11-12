9c0915c9a0330168940e61de29b3cbb9d3e002d0
yasinabdmahmood <abd181103@gmail.com>
2023-11-12 23:02:33 +0300

    Add update action to AgentsController

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -25,5 +25,28 @@ class AgentsController < ApplicationController
         end  
     end
 
+
+    def update
+      name = params["name"]
+      info = params["info"]
+      id = params["id"]
+
+      agent = Agent.find(id);
+      if agent
+        agent.name = name
+        agent.info = info
+        
+        if agent.save
+          render json: agent
+        else
+          render json: {message: 'invalid update'}, status: 400
+        end
+      else
+        render json: {message: 'No such agent exist'}, status: 400
+      end
+      
+
+    end
+
   end
   
