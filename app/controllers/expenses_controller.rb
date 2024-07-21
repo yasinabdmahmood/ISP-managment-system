23a43010bd88f51f0ec729c47ce7d9eca4003eb2
yasinabdmahmood <abd181103@gmail.com>
2024-07-21 09:40:59 +0300

    Add pagination feature to expense controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,7 @@
 class ExpensesController < ApplicationController
   def get_expenses
-    expenses = Expense.all.includes(:account_option)
+    expenses = Expense.order(created_at: :desc).includes(:account_option).page(params[:page]).per(20)
+    # expenses = Expense.all.includes(:account_option)
     render json: expenses, include: { account_option: { only: [:id, :options] } }
   end
 end
