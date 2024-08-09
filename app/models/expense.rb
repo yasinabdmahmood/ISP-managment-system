fde4c13f33ab835664f15af0cddac2b2c494ec8a
yasinabdmahmood <abd181103@gmail.com>
2024-08-09 14:13:05 +0300

    Add validation methods as well as a method to update daily report when expenxe record gets approved

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,9 +1,46 @@
+require 'date'
 class Expense < ApplicationRecord
+    include Report
     belongs_to :account_option
     belongs_to :employee
 
+    after_update  :update_daily_report
+
     validates :amount, presence: true, numericality: true
     validates :date, presence: true
-    validates :remark, presence: true
+    validates :remark, presence: false
     validates :status, presence: true, inclusion: { in: %w[pending rejected approved] }
+    validate :must_have_corresponding_daily_report, on: :update
+    validate :amount_must_not_be_greator_than_trial_balance, on: :update
+
+    private
+
+    def must_have_corresponding_daily_report
+        if status_changed? && status > 'approved'
+            return
+        end
+        expense_date = Time.zone.now
+        daily_report = DailyReport.find_by(created_at: expense_date.beginning_of_day..expense_date.end_of_day)
+        errors.add(:amount, "No corresponding daily report for this expense") if daily_report.nil?
+
+    end
+
+    def amount_must_not_be_greator_than_trial_balance
+        if status_changed? && status > 'approved'
+            return
+        end
+        expense_date = Time.zone.now
+        daily_report = DailyReport.find_by(created_at: expense_date.beginning_of_day..expense_date.end_of_day)
+        trial_balance = daily_report.data['report']['payment_statistics']['trial_balance']
+        errors.add(:amount, "Expense Amount must not be greator than trial balance") if trial_balance < self.amount
+    end
+
+    def update_daily_report
+        if saved_change_to_status? && status == 'approved'
+            expense_date = self.created_at.to_date
+            daily_report = DailyReport.find_by(created_at: expense_date.beginning_of_day..expense_date.end_of_day)
+            add_current_expense_to_belonged_daily_report(self,daily_report)
+        end
+    end
+
 end
