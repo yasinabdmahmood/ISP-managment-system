4a1a0fe78984d5f4541418a15e185be85e88fee0
yasinabdmahmood <abd181103@gmail.com>
2024-10-16 21:11:43 +0300

    Second attemt to fix the pronlem of employee not been able to edit note of subscription

--- app/controllers/subscription_records_controller.rb
+++ app/controllers/subscription_records_controller.rb
@@ -139,7 +139,7 @@ class SubscriptionRecordsController < ApplicationController
       subscription_type: { only: [:category, :cost] } 
     }, status: 200
     else
-      subscription_record.errors.full_messages
+      p subscription_record.errors.full_messages
       render json: {message: 'error'}, status: 400
     end
   end
